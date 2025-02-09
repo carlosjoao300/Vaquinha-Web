cff2926784f1665eca7ac9d38a1ea9c9db499689
carlosjoao300 <132823795+carlosjoao300@users.noreply.github.com>
2025-02-09 15:10:17 -0300

    Add files via upload

--- imagens.js
+++ imagens.js
@@ -7,6 +7,7 @@ let imagemCarro2;
 let imagemCarro3;
 let imagemCarros;
 let imagemMenu;
+let imagemFinal;
 //sons do jogo.
 
 let somDaTrilha;
@@ -20,8 +21,9 @@ function preload(){
   imagemCarro = loadImage("Imagens/carro-1.png");
   imagemCarro2 = loadImage("Imagens/carro-2.png");
   imagemCarro3 = loadImage("Imagens/carro-3.png");
-  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3,imagemCarro, imagemCarro2, imagemCarro3];
   imagemMenu = loadImage("Imagens/imagemMenu.png");
+  imagemFinal = loadImage("Imagens/imagemDoFim.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3,imagemCarro, imagemCarro2, imagemCarro3];
   somDaTrilha = loadSound("sons/trilha.mp3");
   somDaColisao = loadSound("sons/colidiu.mp3"); 
   somDoPonto = loadSound("sons/pontos.wav");
